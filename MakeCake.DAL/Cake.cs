dbfc5aa726dc01ebdd7b1ec14d3ed1c1a6449f82
IRA <brainforfreedom@gmail.com>
2022-10-17 21:35:11 +0300

    Add Cake, Client

---
+++ MakeCake.DAL/Cake.cs
@@ -0,0 +1,14 @@
+﻿namespace MakeCake.DAL
+{
+    public class Cake
+    {
+        public int Id { get; set; }
+        public int Tiers { get; set; } 
+        public int ClientId { get; set; }
+        public DateTime DeadLine { get; set; }
+        public double Price { get; set; }
+        public string? Photo { get; set; }
+        public string? Review { get; set; }
+        public bool IsDead { get; set; }
+    }
+}
